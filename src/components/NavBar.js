fe1c6a6e3193c15402fae78ddb91a2d8b4cd8833
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2023-05-09 19:31:30 +0100

    General Fixes to NavBar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,22 @@
 /*
     Writen By Richard Anderson!
     Project: Personal Portfolio!
-    Last Update: 8th May 2023.
+    Last Update: 9th May 2023.
     Social(s):
         LinkedIn: https://www.linkedin.com/in/richardjanderson9/
         Website: https://portfolio.richardjanderson.uk/
         GitHub: https://github.com/richardjanderson9/
 */
 
-import React from 'react'; // Import React library
-import './assets/css/Navbar.css'; // Import styles from the './Navbar.css' file
-import linksData from './assets/data/navlinks.json'; // Import the JSON data file containing links
+import React from 'react'; // Import React library!
+import './assets/css/Navbar.css'; // Import styles from the './Navbar.css' file!
+import linksData from './assets/data/NavbarLinks.json'; // Import the JSON data file containing links!
 
 const Navbar = () => {
   let { links } = linksData;
 
   return (
-    <nav className="navbar">
+    <nav className="navbar formatting-general">
       <ul className="navbar-list">
         {links.map(({ url, openInNewTab, title }, index) => (
           <li className="navbar-item" key={index}>
