2d18cd9d6dd60b4397fb6105ec1a84fbd780df17
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2023-05-09 20:15:59 +0100

    Footer Improved! Data from JSON and 'rel' added.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,20 +8,21 @@
         GitHub: https://github.com/richardjanderson9/
 */
 
-// Importing styles from other files (CSS)!
+import React from 'react'; // Import React library!
 import '../components/assets/css/App.css'; // Importing styles from the './App.css' file to use across components.
+import footerData from './assets/data/Footer.json'; // Import the JSON data file containing links!
 
+// Defines the Footer component!
 const Footer = () => {
   return (
     <footer className="formatting-footer formatting-general">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <p>&copy; {new Date().getFullYear()} Richard Anderson. All rights reserved.</p>
-            <br />
-            <p><a className="formatting-links" href='https://github.com/richardjanderson9/portfolio-react' target='_blank'>🔗 Source Code! 🔗</a></p>
-          </div>
-        </div>
+      <div>
+        <p>&copy; {footerData?.copyright?.year} {footerData?.copyright?.owner}. {footerData?.copyright?.message}.
+        </p>
+        <br />
+        <p>
+          <a className="formatting-links" href={footerData?.link?.url} target={footerData?.link?.openInNewTab ? "_blank" : "_self"} rel="noreferrer">{footerData?.link?.title}</a>
+        </p>
       </div>
     </footer>
   );
