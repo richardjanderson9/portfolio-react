a00b232c41a624b3c2c067aa0609b3b090407c56
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2023-05-09 20:52:36 +0100

    Added Header!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,30 @@
 /*
     Writen By Richard Anderson!
     Project: Personal Portfolio!
-    Last Update: 7th May 2023.
+    Last Update: 9th May 2023.
     Social(s):
         LinkedIn: https://www.linkedin.com/in/richardjanderson9/
         Website: https://portfolio.richardjanderson.uk/
         GitHub: https://github.com/richardjanderson9/
 */
 
-// Importing styles from other files (CSS)!
-import '../components/assets/css/App.css'; // Importing styles from the './App.css' file to use across components.
+import React from 'react'; // Import React library!
+import { FaLinkedin } from 'react-icons/fa'; // Importing the LinkedIn icon from react-icons library!
+import headerData from './assets/data/Header.json'; // Importing a JSON file that contains data to be displayed in the header!
 
 //Define the Header component!
-function Header() {
-    const data = ("Header!");
-    return (
-      <div>
-        <h1 className="block-general testing-outline">Loaded: {data}</h1>
-      </div>
-    );
-  }
+const Header = () => {
+  const { heading, description, linkedin } = headerData;
+
+  return (
+    <div className="block-general formatting-general formatting-header">
+      {heading.display && <h1 className='text-heading'>{heading.text}</h1>}
+      {description.display && <p className='text-information'>{description.text}</p>}
+      <a href={`${linkedin.url}${linkedin.username}`} className="header-button" target={linkedin.target} rel={linkedin.rel}>
+        <FaLinkedin />
+      </a>
+    </div>
+  );
+};
   
 export default Header; // Export the component for use in other parts of the application!  
